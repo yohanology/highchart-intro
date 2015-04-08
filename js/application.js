7987ce65dcf8a097743e2d9e6056469927ea26b5
Harry Chen <harrychenca@gmail.com>
2015-04-08 10:01:43 +0800

    fix(data): Fix data source

--- js/application.js
+++ js/application.js
@@ -1,42 +1,29 @@
 $(document).ready(function(){
 
-  // data is an array
-  var doneCities = 0;
-  var numCities = 2;
-  var dataHK = [];
-  var dataNYC = [];
-  var urlHK = 'http://api.openweathermap.org/data/2.5/history/city?q=HongKong&type=hour';
-  // var urlNYC = 'http://api.openweathermap.org/data/2.5/history/city?q=nyc&type=hour';
-  var urlNYC = 'http://asdf';
-
-  function getTemp(data, url){
+  function getData(data, url){
     $.ajax({
       type: 'GET',
       url: url,
       dataType: 'JSON',
       success: function(response){
-        // this is how I can get 1 temperature
-        // console.log(response.list[0].main.temp);
+        console.log(response);
 
         // this is how I loop it to get all temperatures
-        $(response.list).each(function(){
+        $(response.data).each(function(){
           // collect each data point 
           var dataPoint = {};
-          dataPoint.y = this.main.temp;
-          dataPoint.x = this.dt * 1000;
+
+          dataPoint.x = new Date(this[0]);
+          dataPoint.y = this[1];
 
           // add each data point to the data array
-          data.push(dataPoint);
+          data.unshift(dataPoint);
         })
 
         // print out data
         console.log(data);
 
-        doneCities++;
-
-        if (doneCities == numCities){
-          initializeHighChart(); 
-        }
+        initializeHighChart();
       },
       error: function(){
         alert("cannot connect");
@@ -44,17 +31,19 @@ $(document).ready(function(){
     });
   }
 
-  getTemp(dataHK, urlHK);
-  getTemp(dataNYC, urlNYC);
+  // data is an array
+  var data = [];
+  var url = 'https://www.quandl.com/api/v1/datasets/BTS_MM/RETAILGAS.json?trim_start=1995-01-02&trim_end=2012-10-15&auth_token=E6kNzExHjay2DNP8pKvB';
+  getData(data, url);
 
   function initializeHighChart(){
     $('#chart').highcharts({
       // key: value
       title: {
-        text: 'Historical Temperatures'
+        text: 'Average retail gas prices'
       },
       subtitle: {
-        text: 'openweathermap.org'
+        text: 'Bureau of Transportation Statistics (Multimodal)'
       },
       xAxis: {
         // Configuration of xAxis
@@ -72,10 +61,10 @@ $(document).ready(function(){
       },
       yAxis: {
         // Configuration of yAxis
-        min: 250,
-        max: 300,
+        // min: 250,
+        // max: 300,
         title: {
-            text: 'Temperature (°K)'
+            text: 'Price ($)'
         }
       },
       legend: {
@@ -87,13 +76,8 @@ $(document).ready(function(){
       },
       series: [{
         // Data points
-        name: 'Hong Kong',
-        data: dataHK
-      },
-      {
-        // Data points
-        name: 'NYC',
-        data: dataNYC
+        name: 'US',
+        data: data
       }]
     });
   }
